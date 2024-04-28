7665787cf55dd6e1870148470fd873438cddf95f
lluuvvii <kurniawanluvi64@gmail.com>
2024-04-28 16:22:22 +0700

    first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import '@styles/global.css'
+
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+import { Suspense } from 'react'
+
+export const metadata = {
+  title: 'Go Promptopia',
+  description: 'Discover & Share AI Prompts'
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang='en'>
+      <body>
+        <Provider>
+          <div className='main'>
+            <div className='gradient' />
+          </div>
+
+          <main className='app'>
+            <Nav />
+            <Suspense fallback={<>Loading...</>}>
+              {children}
+            </Suspense>
+          </main>
+        </Provider>
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
